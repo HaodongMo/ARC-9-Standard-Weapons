101c1743021871661161faab1bd14ce99b0f70b3
Haodong Mo <mohaodong@gmail.com>
2022-01-19 12:27:14 +1000

    Gotta have stats

--- lua/arc9/common/attachments/m16_barrel_ris.lua
+++ lua/arc9/common/attachments/m16_barrel_ris.lua
@@ -2,7 +2,7 @@ ATT.PrintName = "RAI RIS 20\" Barrel"
 ATT.CompactName = "20\"RIS"
 ATT.Description = [[Standard barrel shrouded by a quad-rail RIS handguard.
 
-Allows for easy mounting of tactical devices and grips, but is otherwise functionally identical to a ribbed handguard.]]
+Allows for easy mounting of tactical devices and grips. Rail interface is less comfortable than a standard handguard.]]
 
 ATT.PrintName_TrueName = "Colt M16A4 RIS 20\" Barrel"
 
@@ -34,3 +34,5 @@ ATT.Attachments = {
         Icon_Offset = Vector(6, 2, 0)
     },
 }
+
+ATT.RecoilAutoControlMult = 0.95
