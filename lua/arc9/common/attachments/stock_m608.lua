e87cd5fbc4150e46053873afd56cf6b2d9f91e00
Haodong Mo <mohaodong@gmail.com>
2022-01-19 17:14:07 +1000

    Added M608 stock

---
+++ lua/arc9/common/attachments/stock_m608.lua
@@ -0,0 +1,30 @@
+ATT.PrintName = "JTAC Slim Pad"
+ATT.CompactName = "SLIM"
+ATT.Description = [[Extremely short stock for buffer tube stocks.
+
+Poor recoil and sway mitigation, but helps a lot with point shooting.]]
+
+ATT.PrintName_TrueName = "M608 Survival Rifle Pad"
+ATT.CompactName_TrueName = "608"
+ATT.Description_TrueName = [[Extremely short stock for buffer tube stocks.
+
+Poor recoil and sway mitigation, but helps a lot with point shooting.]]
+
+ATT.Icon = Material("entities/arc9_att_stock_m608.png", "mips smooth")
+
+ATT.SortOrder = 0
+ATT.Category = "bt_stock"
+
+ATT.Model = "models/weapons/arc9/atts/stock_608.mdl"
+
+ATT.SwayAdd = -0.87
+ATT.SprintToFireTimeMult = 1.34
+ATT.AimDownSightsTimeMult = 1.26
+
+ATT.RecoilMult = 0.90
+ATT.RecoilKickMult = 1.26
+ATT.RecoilAutoControlMult = 3.5
+
+ATT.FreeAimRadiusMult = 0.667
+
+ATT.ModelOffset = Vector(-3.5, 0, 0)
