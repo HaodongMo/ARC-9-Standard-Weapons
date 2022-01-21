039a88cf6260cf977b0722c9988cd64334f57622
Haodong Mo <mohaodong@gmail.com>
2022-01-21 16:26:52 +1000

    Added Gooseneck

---
+++ lua/arc9/common/attachments/mount_gooseneck.lua
@@ -0,0 +1,26 @@
+ATT.PrintName = "PSM Gooseneck Mount"
+ATT.CompactName = "GOOSENECK"
+ATT.Description = "Device for attaching picatinny standard options to any RAY type carrying handle while co-witnessing the standard irons."
+
+ATT.PrintName_TrueName = "M16 Gooseneck Rail"
+ATT.CompactName_TrueName = "GOOSENECK"
+ATT.Description_TrueName = "Device for attaching picatinny standard options to any M16 type carrying handle while co-witnessing the standard irons."
+
+ATT.Icon = Material("entities/arc9_att_mount_gooseneck.png", "mips smooth")
+
+ATT.SortOrder = 0
+ATT.Category = "mount_m16ch"
+ATT.Model = "models/weapons/arc9/atts/mount_gooseneck.mdl"
+
+ATT.Attachments = {
+    {
+        PrintName = "OPTIC",
+        Category = "optic_picatinny_medium", // single or {"list", "of", "values"}
+        Pos = Vector(-7.45884, 0, 0.85),
+        Ang = Angle(0, 0, 0),
+        Icon_Offset = Vector(0, 1, 0),
+        ExtraSightDistance = 10
+    }
+}
+
+ATT.SwayAdd = 0.05
