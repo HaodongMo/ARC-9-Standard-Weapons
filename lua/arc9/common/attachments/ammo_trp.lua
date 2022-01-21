4b15eabcadadcbc5fcb1df2ca62fdfa72864e2f1
Haodong Mo <mohaodong@gmail.com>
2022-01-21 17:09:15 +1000

    Pink tracer

---
+++ lua/arc9/common/attachments/ammo_trp.lua
@@ -0,0 +1,22 @@
+ATT.PrintName = "Pink Tracer"
+ATT.CompactName = "TR-P"
+ATT.Icon = Material("entities/arc9_att_ammo.png", "mips smooth")
+ATT.Description = [[Tracer rounds. One tracer is loaded every 3 shots, and the last 5 shots are all tracers.]]
+ATT.SortOrder = 0
+
+ATT.Category = {"ammo"}
+ATT.Folder = "tracer/monocolor"
+
+ATT.TracerNum = 3
+ATT.TracerFinalMag = 5
+ATT.TracerColor = Color(255, 25, 255)
+
+ATT.TracerSizeAdd = 5
+
+ATT.Hook_BulletImpact = function(swep, data)
+    local tr = data.tr
+
+    if math.Rand(0, 100) < data.dmgv * 0.25 then
+        tr.Entity:Ignite(0.25, 8)
+    end
+end
