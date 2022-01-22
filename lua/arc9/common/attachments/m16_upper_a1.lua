3410cfbc17de4146a2e03ec8c8896d7a39449a4a
Haodong Mo <mohaodong@gmail.com>
2022-01-22 11:20:38 +1000

    Fixed description for auto uppers

--- lua/arc9/common/attachments/m16_upper_a1.lua
+++ lua/arc9/common/attachments/m16_upper_a1.lua
@@ -1,10 +1,10 @@
 ATT.PrintName = "RAY/CAR-83 Auto Upper"
 ATT.CompactName = "RC83AUTO"
-ATT.Description = [[Experimental upper receiver that enables fully automatic fire. Increases fire rate and recoil.]]
+ATT.Description = [[Experimental upper receiver that enables fully automatic fire, at the cost of significantly worse recoil characteristics.]]
 
 ATT.PrintName_TrueName = "Colt M16A1 Upper"
 ATT.CompactName_TrueName = "A1UPPER"
-ATT.Description_TrueName = [[Classic upper receiver that enables fully automatic fire. Increases fire rate and recoil.]]
+ATT.Description_TrueName = [[Classic upper receiver that enables fully automatic fire, at the cost of significantly worse recoil characteristics.]]
 
 ATT.Icon = Material("entities/arc9_att_m16_upper_a1.png", "mips smooth")
 
--- lua/arc9/common/attachments/m16_upper_a3.lua
+++ lua/arc9/common/attachments/m16_upper_a3.lua
@@ -1,10 +1,10 @@
 ATT.PrintName = "RAY/CAR-83 Flat Top Auto Upper"
 ATT.CompactName = "RC83FTA"
-ATT.Description = [[Experimental upper receiver that enables fully automatic fire and comes with a picatinny rail. Increases fire rate and recoil.]]
+ATT.Description = [[Experimental upper receiver that enables fully automatic fire, at the cost of significantly worse recoil characteristics.]]
 
 ATT.PrintName_TrueName = "Colt M16A3 Railed Upper"
 ATT.CompactName_TrueName = "A3UPPER"
-ATT.Description_TrueName = [[Railed upper receiver that enables fully automatic fire. Increases fire rate and recoil.]]
+ATT.Description_TrueName = [[Railed upper receiver that enables fully automatic fire, at the cost of significantly worse recoil characteristics.]]
 
 ATT.Icon = Material("entities/arc9_att_m16_upper_a4.png", "mips smooth")
 
