c44d72f431688bd3fab430ef699debe1e31682d5
TheOnly8Z <888888zombies@gmail.com>
2022-01-22 23:44:25 -0500

    nailgun :trollscream:

---
+++ lua/arc9/common/attachments/ammo_nail.lua
@@ -0,0 +1,25 @@
+ATT.PrintName = "Nails"
+ATT.CompactName = "NAIL"
+ATT.Icon = Material("entities/arc9_att_ammo.png", "mips smooth")
+ATT.Description = [[i will never forgive valve for removing the nailgun from team fortress two]]
+ATT.SortOrder = 0
+
+ATT.Category = {"ammo_assault", "ammo_sniper"}
+
+ATT.PhysBulletMuzzleVelocityOverride = 2000
+ATT.PhysBulletGravityOverride = 0.5
+ATT.PhysBulletDragOverride = 0.25
+
+ATT.RecoilMult = 0.25
+
+ATT.DamageMaxMult = 0.5
+ATT.DamageMinMult = 0.5
+
+ATT.SpreadAdd = 0.015
+
+ATT.PenetrationMult = 0.25
+ATT.RicochetChanceMult = 0.25
+ATT.ArmorPiercingAdd = 0.1
+
+ATT.PhysBulletModelOverride = "models/weapons/w_models/w_nail.mdl"
+ATT.PhysBulletModelStickOverride = 10
