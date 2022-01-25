2eb961163b80adf0931f627659f4656a073483e9
TheOnly8Z <888888zombies@gmail.com>
2022-01-25 12:32:47 -0500

    description for troy grip

--- lua/arc9/common/attachments/grip_vertical.lua
+++ lua/arc9/common/attachments/grip_vertical.lua
@@ -1,6 +1,6 @@
 ATT.PrintName = "Troy VFG"
 ATT.CompactName = "TROY"
-ATT.Description = [[Wooden grip made by Californian gunsmiths without access to advanced polymers.]]
+ATT.Description = [[Rail-mounted vertical foregrip. Provides a stable, vertical surface to grip onto, improving recoil control and reducing recoil drift.]]
 
 ATT.Icon = Material("entities/arc9_att_m16_grip_wood.png", "mips smooth")
 
@@ -9,8 +9,8 @@ ATT.Model = "models/weapons/arccw/atts/uc_troygrip.mdl"
 ATT.SortOrder = 0
 ATT.Category = "grip_picatinny"
 
-ATT.RecoilPatternDriftMult = 0.9
-ATT.RecoilAutoControlMult = 1.25
+ATT.RecoilPatternDriftMult = 0.75
+ATT.RecoilAutoControlMult = 1.15
 
 ATT.LHIK = true
 
@@ -19,5 +19,5 @@ ATT.ModelAngleOffset = Angle(0, 0, 180)
 
 ATT.SwayAdd = 0.1
 ATT.AimDownSightsTimeMult = 1.05
-ATT.SprintToFireTimeMult = 1.05
+ATT.SprintToFireTimeMult = 1.075
 ATT.SpeedMult = 0.97
