6c2876e21c09518f7935227a19e7e346096695d0
Haodong Mo <mohaodong@gmail.com>
2022-02-13 00:41:35 +1000

    Create m16_mag_bottomless.lua

---
+++ lua/arc9/common/attachments/m16_mag_bottomless.lua
@@ -0,0 +1,19 @@
+ATT.PrintName = "Lesser Patriot Drum"
+ATT.CompactName = "BOTTOMLESS"
+ATT.Icon = Material("entities/arc9_att_m16_mag_100.png", "mips smooth")
+ATT.Description = [[An inferior replica of The Boss's infinite ammunition drum magazine. Though it is capable of holding an unlimited quantity of ammunition, meaning it never needs to be reloaded (though it can be if the user desires), it still requires ammunition to load in order to fire.]]
+
+ATT.ClipSizeOverride = 100
+
+ATT.SortOrder = 9998
+
+ATT.AdminOnly = true
+
+ATT.Category = "m16_mag"
+
+ATT.DropMagazineModel = "models/weapons/arc9/droppedmags/m16_mag_100.mdl"
+
+ATT.BottomlessClip = true
+ATT.InfiniteAmmo = false
+
+ATT.ActivateElements = {"m16_mag_100"}
