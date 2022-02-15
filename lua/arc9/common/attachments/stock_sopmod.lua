4d373ef5e1879c20e1d5f15711447e2c647db943
Haodong Mo <mohaodong@gmail.com>
2022-02-15 18:53:14 +1000

    Added LMT SOPMOD stock

---
+++ lua/arc9/common/attachments/stock_sopmod.lua
@@ -0,0 +1,31 @@
+ATT.PrintName = "PSM Contract2"
+ATT.CompactName = "CONTRACT2"
+ATT.Description = [[Sturdy, heavy stock for buffer tube adapters.
+
+Good sway mitigation, but poor point firing.]]
+
+ATT.PrintName_TrueName = "LMT SOPMOD"
+ATT.CompactName_TrueName = "M4SS"
+ATT.Description_TrueName = [[Sturdy, heavy stock for buffer tube adapters.
+
+Good sway mitigation, but poor point firing.]]
+
+ATT.Icon = Material("entities/arc9_att_stock_sopmod.png", "mips smooth")
+
+ATT.SortOrder = 0
+ATT.Category = "bt_stock"
+
+ATT.Model = "models/weapons/arc9/atts/stock_sopmod.mdl"
+
+ATT.SpeedMult = 0.98
+
+ATT.SwayAdd = -0.75
+ATT.SwayMult = 0.85
+ATT.SprintToFireTimeMult = 1.44
+ATT.AimDownSightsTimeMult = 1.33
+
+ATT.RecoilMult = 0.7
+ATT.RecoilKickMult = 1.42
+ATT.RecoilAutoControlMult = 4
+
+ATT.FreeAimRadiusMult = 0.9
