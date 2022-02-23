fbfcd376f4afc6e93e7a0012cee37ad41f868375
OpticaJr <rcharge617@gmail.com>
2022-02-23 11:48:52 +0300

    Revert "Revert "Merge branch 'main' of https://github.com/HaodongMo/ARC-9-Standard-Weapons""
    This reverts commit 9b7280c00a41feab76c2d65013648f02f65df452.

---
+++ lua/arc9/common/attachments/mp5_barrel_old.lua
@@ -0,0 +1,14 @@
+ATT.PrintName = "DWF Classic 9\" Barrel"
+ATT.CompactName = "9\"CLASSIC"
+ATT.Description = [[Classic handguard design used in early production runs. Lighter than the modern version, but with worse recoil control.]]
+
+ATT.PrintName_TrueName = "MP5 Early 9\" Barrel"
+ATT.CompactName_TrueName = "9\"EARLY"
+
+ATT.Icon = Material("entities/arc9_att_mp5_barrel_old.png", "mips smooth")
+
+ATT.SortOrder = 0
+ATT.Category = "mp5_barrel"
+
+ATT.AimDownSightsTimeMult = 0.97
+ATT.RecoilAutoControlMult = 0.96
---
+++ lua/arc9/common/attachments/mp5_stock_pdw.lua
@@ -0,0 +1,21 @@
+ATT.PrintName = "DWF Ranger Stock"
+ATT.CompactName = "RANGER"
+ATT.Description = [[Lightweight personal defense weapon stock. Good hip fire characteristics.]]
+
+ATT.PrintName_TrueName = "MP5K PDW Folding Stock"
+ATT.CompactName_TrueName = "PDW"
+
+ATT.Icon = Material("entities/arc9_att_mp5_stock_pdw.png", "mips smooth")
+
+ATT.SortOrder = 0
+ATT.Category = "mp5_stock"
+
+ATT.SwayAdd = -0.87
+ATT.SprintToFireTimeMult = 1.34
+ATT.AimDownSightsTimeMult = 1.26
+
+ATT.RecoilMult = 0.90
+ATT.RecoilKickMult = 1.26
+ATT.RecoilAutoControlMult = 3.5
+
+ATT.FreeAimRadiusMult = 0.6
