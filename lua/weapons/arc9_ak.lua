c7a8b55a9c3dad5c1bf7c2cfbde7cc53afe396fb
Haodong Mo <mohaodong@gmail.com>
2022-03-23 21:20:34 +1100

    Removed VSS

--- lua/weapons/arc9_ak.lua
+++ lua/weapons/arc9_ak.lua
@@ -95,6 +95,7 @@ SWEP.DropMagazineSounds = {
     "weapons/arc9_ud/common/rifle_magdrop_1.ogg", "weapons/arc9_ud/common/rifle_magdrop_2.ogg", "weapons/arc9_ud/common/rifle_magdrop_3.ogg", "weapons/arc9_ud/common/rifle_magdrop_4.ogg"
 } -- Table of sounds a dropped magazine should play.
 SWEP.DropMagazineAmount = 1 -- Amount of mags to drop.
+SWEP.DropMagazineTime = 1
 
 -------------------------- FIREMODES
 
