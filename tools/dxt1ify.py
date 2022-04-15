09c30e26c75cce027f7a9e3aeff12d21efbabfaa
Haodong Mo <mohaodong@gmail.com>
2022-04-15 18:33:35 +1000

    Non-Alpha-discarding function

--- tools/dxt1ify.py
+++ tools/dxt1ify.py
@@ -10,9 +10,23 @@ from VTFLibWrapper.VTFLibEnums import ImageFormat
 
 # Edit these variables
 PATH_TO_DIR = "materials/"
-CLAMP_SIZE = 2048
 
 vtf_lib = VTFLib.VTFLib()
+
+def has_transparency(img):
+    if image.info.get("transparency", None) is not None:
+        return True
+    if img.mode == "P":
+        transparent = img.info.get("transparency", -1)
+        for _, index in img.getcolors():
+            if index == transparent:
+                return True
+    elif img.mode == "RGBA":
+        extrema = img.getextrema()
+        if extrema[3][0] < 255:
+            return True
+
+    return False
 
 for path, subdirs, files in os.walk(PATH_TO_DIR):
     for name in files:
@@ -23,47 +37,28 @@ for path, subdirs, files in os.walk(PATH_TO_DIR):
 
             w = vtf_lib.width()
             h = vtf_lib.height()
-            neww = w
-            newh = h
             format = vtf_lib.image_format()
 
-            # print(filepath)
-            scale = 1
-            if w > CLAMP_SIZE or h > CLAMP_SIZE:
-                maxd = max(w, h)
-                scale = CLAMP_SIZE / maxd
-                neww *= scale
-                newh *= scale
-
-            # print(format.name)
-
-            if scale != 1 or format not in (ImageFormat.ImageFormatDXT1, ImageFormat.ImageFormatDXT5):
+            if format != ImageFormat.ImageFormatDXT1:
                 image_full = vtf_lib.image_load(filepath, False)
                 def_options = vtf_lib.create_default_params_structure()
                 image_data = vtf_lib.get_rgba8888()
                 image_data = bytes(image_data.contents)
-
                 image = Image.frombytes("RGBA", (w, h), image_data)
-                r, g, b, a = image.split()
 
-                if scale != 1:
-                    image_scaled = image.resize((int(neww), int(newh)))
-                    image_a_scaled = a.resize((int(neww), int(newh)))
-                    r,g,b = image_scaled.split()
-                    colorImage = (r, g, b, image_a_scaled)
-                    image = Image.merge('RGBA', colorImage)
+                should_dxt1 = has_transparency(image)
 
                 image_data = (np.asarray(image)*-1) * 255
                 image_data = image_data.astype(np.uint8, copy=False)
                 image_data = create_string_buffer(image_data.tobytes())
 
-                def_options.ImageFormat = ImageFormat.ImageFormatDXT5
-                def_options.Flags |= VTFLibEnums.ImageFlag.ImageFlagEightBitAlpha
                 def_options.Resize = 1
 
-                vtf_lib.image_create_single(int(neww), int(newh), image_data, def_options)
-                vtf_lib.image_save(filepath)
-                print("Converted", filepath, "successfully.")
+                if should_dxt1:
+                    def_options.ImageFormat = ImageFormat.ImageFormatDXT1
+                    vtf_lib.image_create_single(w, h, image_data, def_options)
+                    vtf_lib.image_save(filepath)
+                    print("DXT1-ified", filepath, "successfully.")
 
 
 
