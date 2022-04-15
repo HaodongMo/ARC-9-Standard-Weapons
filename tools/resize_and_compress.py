2881c662c80de39ae594648eb3e6052a1bdc8416
Haodong Mo <mohaodong@gmail.com>
2022-04-15 18:13:35 +1000

    Fully working version !!!

--- tools/resize_and_compress.py
+++ tools/resize_and_compress.py
@@ -12,6 +12,21 @@ PATH_TO_DIR = "materials/"
 
 vtf_lib = VTFLib.VTFLib()
 
+def has_transparency(img):
+    if image.info.get("transparency", None) is not None:
+        return True
+    if img.mode == "P":
+        transparent = img.info.get("transparency", -1)
+        for _, index in img.getcolors():
+            if index == transparent:
+                return True
+    elif img.mode == "RGBA":
+        extrema = img.getextrema()
+        if extrema[3][0] < 255:
+            return True
+
+    return False
+
 for path, subdirs, files in os.walk(PATH_TO_DIR):
     for name in files:
         filepath = os.path.join(path, name)
@@ -38,22 +53,22 @@ for path, subdirs, files in os.walk(PATH_TO_DIR):
             if scale != 1 or format not in (ImageFormat.ImageFormatDXT1, ImageFormat.ImageFormatDXT5):
                 image_full = vtf_lib.image_load(filepath, False)
                 def_options = vtf_lib.create_default_params_structure()
-                def_options.ImageFormat = ImageFormat.ImageFormatDXT5
-                if scale != 1:
-                    image_data = vtf_lib.get_rgba8888()
-                    image_data = bytes(image_data.contents)
+                image_data = vtf_lib.get_rgba8888()
+                image_data = bytes(image_data.contents)
+                image = Image.frombytes("RGBA", (w, h), image_data)
 
-                    image = Image.frombytes("RGBA", (w, h), image_data)
+                if scale != 1:
                     image = image.resize((int(neww), int(newh)))
 
-                    image_data = (np.asarray(image)*-1) * 255
-                    image_data = image_data.astype(np.uint8, copy=False)
-                    image_data = create_string_buffer(image_data.tobytes())
-                    vtf_lib.image_create_single(int(neww), int(newh), image_data, def_options)
-                # elif format not in (ImageFormat.ImageFormatDXT1, ImageFormat.ImageFormatDXT5):
-                converted_image = vtf_lib.convert(ImageFormat.ImageFormatDXT5).contents
-                # converted_image = bytes(converted_image.contents)
-                vtf_lib.image_create_single(int(neww), int(newh), converted_image, def_options)
+                image_data = (np.asarray(image)*-1) * 255
+                image_data = image_data.astype(np.uint8, copy=False)
+                image_data = create_string_buffer(image_data.tobytes())
+
+                def_options.ImageFormat = ImageFormat.ImageFormatDXT5
+                def_options.Flags |= VTFLibEnums.ImageFlag.ImageFlagEightBitAlpha
+                def_options.Resize = 1
+
+                vtf_lib.image_create_single(int(neww), int(newh), image_data, def_options)
                 vtf_lib.image_save(filepath)
                 print("Converted", filepath, "successfully.")
 
